477f20d468dddb682056ae16d25aecd9a1b5a076
DrinoSan <s.becirbegovic@gmail.com>
2023-10-15 20:30:48 +0200

    Rollback: Fucked up last commit this is the rollback or something like that

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -54,7 +54,6 @@ lsp.setup_nvim_cmp({
 })
 
 
-
 lsp.on_attach(function(client, bufnr)
     print("SAND LSP RUNNING")
     local opts = { buffer = bufnr, remap = false }
@@ -76,35 +75,8 @@ lsp.on_attach(function(client, bufnr)
     end, opts)
 end)
 
+lsp.setup()
 
-require('lspconfig').clangd.setup({
-    cmd = {
-        "/Users/sandi/.local/share/nvim/mason/bin/clangd",
-        "--header-insertion=never",
-    },
-  single_file_support = true,
-  filetype = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-  on_attach = function(client, bufnr)
-    print('hello Funcking clangd')
-    local opts = { buffer = bufnr, remap = false }
-
-    vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
-    vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-  end
-})
 
 vim.diagnostic.config({
     virtual_text = true,
