8f9bbc0fdc0490363a704ca046909778fa9a6521
DrinoSan <s.becirbegovic@gmail.com>
2024-02-04 20:41:07 +0100

    Removed dead code

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -76,32 +76,3 @@ vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
-
-
--- Define a custom telescope command to list files in $HOME/.config
--- function List_config_files()
---     local config_dir = os.getenv('HOME') .. '/.config/nvim'
---     local find_command = {
---         'find', config_dir, '-type', 'f', '-print'
---     }
-
---     require 'telescope.builtin'.find_files {
---         find_command = find_command,
---         prompt_title = 'Config Files',
---     }
--- end
-
--- local builtin = require('telescope.builtin')
--- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- vim.keymap.set('n', '<C-f>', List_config_files, {})
--- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
--- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
--- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
--- vim.keymap.set('n', '<leader>qf', builtin.quickfix, {})
--- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- vim.keymap.set('n', '<leader>col', builtin.colorscheme, {})
--- vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
--- vim.keymap.set('n', '<leader>fss', builtin.lsp_document_symbols, {})
--- vim.keymap.set('n', '<leader>ps', function()
---     builtin.grep_string({ search = vim.fn.input("Grep > ") });
--- end)
