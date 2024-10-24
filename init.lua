bdf517cbf12d3074b8bfc98a9348246cd9dc362e
DrinoSan <s.becirbegovic@gmail.com>
2024-10-24 22:04:22 +0200

    std 20

--- init.lua
+++ init.lua
@@ -564,7 +564,11 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
+        clangd = {
+          init_options = {
+            fallbackFlags = { '--std=c++20' },
+          },
+        },
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
